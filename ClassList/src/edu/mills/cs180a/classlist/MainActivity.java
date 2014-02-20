8e52a6c6ff4a1de14c43c10d2d48a681cad26540
ctaymor <ctaymor@gmail.com>
2014-02-20 00:33:29 -0800

    Build custom adapter, not working Added class IconicAdapter which extends ArrayAdapter<Person>. Also made override for IconicAdapter's getView method.
    Not compiling yet: Program is trying to access android.R.layout.* where
    I want it to access the generated R.layout.* It is having the same problem
    with R.id.* vs android.R.id.*
    
    Committing so that the work is saved before rolling back to previous commit
    to try to understand where the problem arose.

--- ClassList/src/edu/mills/cs180a/classlist/MainActivity.java
+++ ClassList/src/edu/mills/cs180a/classlist/MainActivity.java
@@ -1,12 +1,15 @@
 
 package edu.mills.cs180a.classlist;
 
+import android.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -15,8 +18,7 @@ public class MainActivity extends Activity implements ListView.OnItemClickListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayAdapter<Person> adapter = new ArrayAdapter<Person>(this,
-                android.R.layout.simple_list_item_1, Person.everyone);
+        IconicAdapter adapter = new IconicAdapter();
         ListView listView = (ListView) findViewById(R.id.listView1);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(this);
@@ -31,5 +33,21 @@ public class MainActivity extends Activity implements ListView.OnItemClickListen
         String [] emails = {Person.everyone[position].getEmail()};
         intent.putExtra(Intent.EXTRA_EMAIL, emails);
         startActivity(Intent.createChooser(intent, "Send Email"));
+    }
+    
+    class IconicAdapter extends ArrayAdapter<Person> {
+        IconicAdapter() {
+            super(MainActivity.this, R.layout.row, Person.everyone);
+        }
+        
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View row = super.getView(position, convertView, parent);
+            ImageView icon = (ImageView) row.findViewById(R.id.icon);
+            
+            icon.setImageResource(Person.everyone[position].getImage());
+            
+            return row;
+        }
     }
 }
